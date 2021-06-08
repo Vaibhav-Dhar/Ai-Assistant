ab75a7c19c00665ed1ebd876af09daa2706f2678
Vaibhav Dhar <73923750+Vaibhav-Dhar@users.noreply.github.com>
2021-06-08 13:37:20 +0530

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -6,39 +6,39 @@ import webbrowser
 engine=pyttsx3.init()
 voices=engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-newVoiceRate=180
+newVoiceRate=175
 engine.setProperty('rate',newVoiceRate)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-#def wishme():
-#    speak("Welcome master!")
-#    speak("This is Tokyo your AI assistant")
-#    # speak("Today's date is")
-#    
-#    # year=int(datetime.datetime.now().year)
-#    # month=int(datetime.datetime.now().month)
-#    # date=int(datetime.datetime.now().day)
-#    # speak(date)
-#    # speak(month)
-#    # speak(year)
-#    # speak("The current time in my watch is")
-#
-#    # Time=datetime.datetime.now().strftime("%H:%M:%S")
-#    # speak(Time)
-#    
-#    hour=int(datetime.datetime.now().hour)
-#    if hour>=6 and hour<=12:
-#        speak("Good Morning sir!")
-#    elif hour>=12 and hour<=18:
-#        speak("Good Afternoon sir!")
-#    elif hour>=18 and hour<=24:
-#        speak("Good Evening Sir!")
-#    else:
-#        speak("Good Night Sir!")   
-#
-#    speak("I am at your service. How may i help you?")               
+def wishme():
+    speak("Welcome master!")
+    speak("This is Tokyo your AI assistant")
+    # speak("Today's date is")
+    
+    # year=int(datetime.datetime.now().year)
+    # month=int(datetime.datetime.now().month)
+    # date=int(datetime.datetime.now().day)
+    # speak(date)
+    # speak(month)
+    # speak(year)
+    # speak("The current time in my watch is")
+
+    # Time=datetime.datetime.now().strftime("%H:%M:%S")
+    # speak(Time)
+    
+    hour=int(datetime.datetime.now().hour)
+    if hour>=6 and hour<=12:
+        speak("Good Morning sir!")
+    elif hour>=12 and hour<=18:
+        speak("Good Afternoon sir!")
+    elif hour>=18 and hour<=24:
+        speak("Good Evening Sir!")
+    else:
+        speak("Good Night Sir!")   
+
+    speak("I am at your service. How may i help you?")               
 
 def takecommand():
     r=sr.Recognizer()
@@ -56,11 +56,13 @@ def takecommand():
         speak("Please repeat that one more time...")
         return"None"
     return query
-if __name__=="__main__":
-#    wishme()
     
+if __name__=="__main__":
+    wishme()
+    
+    while True:
         query=takecommand().lower()
-        webbrowser = webbrowser.get('safari')
+
         if "wikipedia" in query:
             speak("Searching Wikipedia...")
             query=query.replace("Wikipedia","")
