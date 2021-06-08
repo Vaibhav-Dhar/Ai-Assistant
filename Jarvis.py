38915d8ee51032bb41a385e9c8168d2723d88334
Vaibhav Dhar <73923750+Vaibhav-Dhar@users.noreply.github.com>
2021-06-08 12:04:06 +0530

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -4,7 +4,7 @@ import speech_recognition as sr
 engine=pyttsx3.init()
 voices=engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-newVoiceRate=170
+newVoiceRate=180
 engine.setProperty('rate',newVoiceRate)
 def speak(audio):
     engine.say(audio)
@@ -59,6 +59,7 @@ takecommand()
 if __name__=="__main__":
     wishme()
     takecommand()
+
 
 
 
