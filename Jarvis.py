506b29d3e39d7d2c96545556c555d10df8bbbb9a
Vaibhav Dhar <73923750+Vaibhav-Dhar@users.noreply.github.com>
2021-06-08 11:03:43 +0530

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -10,34 +10,34 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-# def wishme():
-#     speak("Welcome master!")
-#     speak("This is Tokyo your AI assistant")
-#     speak("Today's date is")
+def wishme():
+    speak("Welcome master!")
+    speak("This is Tokyo your AI assistant")
+    speak("Today's date is")
     
-#     year=int(datetime.datetime.now().year)
-#     month=int(datetime.datetime.now().month)
-#     date=int(datetime.datetime.now().day)
-#     speak(date)
-#     speak(month)
-#     speak(year)
-#     speak("The current time in my watch is")
+    year=int(datetime.datetime.now().year)
+    month=int(datetime.datetime.now().month)
+    date=int(datetime.datetime.now().day)
+    speak(date)
+    speak(month)
+    speak(year)
+    speak("The current time in my watch is")
 
-#     Time=datetime.datetime.now().strftime("%H:%M:%S")
-#     speak(Time)
+    Time=datetime.datetime.now().strftime("%H:%M:%S")
+    speak(Time)
     
-#     hour=int(datetime.datetime.now().hour)
-#     if hour>=6 and hour<=12:
-#         speak("Good Morning sir!")
-#     elif hour>=12 and hour<=18:
-#         speak("Good Afternoon sir!")
-#     elif hour>=18 and hour<=24:
-#         speak("Good Evening Sir!")
-#     else:
-#         speak("Good Night Sir!")   
+    hour=int(datetime.datetime.now().hour)
+    if hour>=6 and hour<=12:
+        speak("Good Morning sir!")
+    elif hour>=12 and hour<=18:
+        speak("Good Afternoon sir!")
+    elif hour>=18 and hour<=24:
+        speak("Good Evening Sir!")
+    else:
+        speak("Good Night Sir!")   
 
-#     speak("I am at your service. How may i help you?")               
-# wishme()
+    speak("I am at your service. How may i help you?")               
+wishme()
 
 def takecommand():
     r=sr.Recognizer()
