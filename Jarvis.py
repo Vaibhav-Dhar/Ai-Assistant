71952dfd03d57de7086c785d10bc4df62ad29f6c
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-06-08 13:33:19 +0530

    Added chrome web

--- Jarvis.py
+++ Jarvis.py
@@ -12,33 +12,33 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-def wishme():
-    speak("Welcome master!")
-    speak("This is Tokyo your AI assistant")
-    # speak("Today's date is")
-    
-    # year=int(datetime.datetime.now().year)
-    # month=int(datetime.datetime.now().month)
-    # date=int(datetime.datetime.now().day)
-    # speak(date)
-    # speak(month)
-    # speak(year)
-    # speak("The current time in my watch is")
-
-    # Time=datetime.datetime.now().strftime("%H:%M:%S")
-    # speak(Time)
-    
-    hour=int(datetime.datetime.now().hour)
-    if hour>=6 and hour<=12:
-        speak("Good Morning sir!")
-    elif hour>=12 and hour<=18:
-        speak("Good Afternoon sir!")
-    elif hour>=18 and hour<=24:
-        speak("Good Evening Sir!")
-    else:
-        speak("Good Night Sir!")   
-
-    speak("I am at your service. How may i help you?")               
+#def wishme():
+#    speak("Welcome master!")
+#    speak("This is Tokyo your AI assistant")
+#    # speak("Today's date is")
+#    
+#    # year=int(datetime.datetime.now().year)
+#    # month=int(datetime.datetime.now().month)
+#    # date=int(datetime.datetime.now().day)
+#    # speak(date)
+#    # speak(month)
+#    # speak(year)
+#    # speak("The current time in my watch is")
+#
+#    # Time=datetime.datetime.now().strftime("%H:%M:%S")
+#    # speak(Time)
+#    
+#    hour=int(datetime.datetime.now().hour)
+#    if hour>=6 and hour<=12:
+#        speak("Good Morning sir!")
+#    elif hour>=12 and hour<=18:
+#        speak("Good Afternoon sir!")
+#    elif hour>=18 and hour<=24:
+#        speak("Good Evening Sir!")
+#    else:
+#        speak("Good Night Sir!")   
+#
+#    speak("I am at your service. How may i help you?")               
 
 def takecommand():
     r=sr.Recognizer()
@@ -57,11 +57,10 @@ def takecommand():
         return"None"
     return query
 if __name__=="__main__":
-    wishme()
+#    wishme()
     
-    while True:
         query=takecommand().lower()
-
+        webbrowser = webbrowser.get('safari')
         if "wikipedia" in query:
             speak("Searching Wikipedia...")
             query=query.replace("Wikipedia","")
