6314605a79bad3230964da56e3e071a5b0d92b45
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-06-21 16:24:53 +0530

    Create headers.py

---
+++ headers.py
@@ -0,0 +1,10 @@
+import os
+
+try:
+    os.system('pip install pyttsx3')
+    os.system('pip install speechrecognition')
+    os.system('pip install wikipedia')
+    os.system('pip install selenium')
+    os.system('pip install pyautogui')
+    os.system('pip install ')
+    os.system('pip install pyttsx3')
